b5ce5d3b3589ee3de6b01cde6f0f79086bf88317
anmolkatak <anmol0118@gmail.com>
2022-01-15 00:06:50 -0500

    rm

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-bucket-for-tf-state"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "for_state_lock"
-    encrypt = true
-  }
-}
