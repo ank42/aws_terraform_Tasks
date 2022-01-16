0ae1489221fd21f8794fb4dbf1928da62991b66c
anmolkatak <anmol0118@gmail.com>
2022-01-16 12:06:27 -0500

    Port update 80

--- l1/instance.tf
+++ l1/instance.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = ["100.1.180.196/32"]
   }
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["100.1.180.196/32"]
+  }
 
   ingress {
     description = "Allow internal traffic"
