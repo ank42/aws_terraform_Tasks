89dc6fb7ed5e11c68b8516b8c4c75c490a7d35c5
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2022-01-19 09:23:28 -0500

    Fix corss-state communication

--- l2/backend.tf
+++ l2/backend.tf
@@ -7,12 +7,3 @@ terraform {
     encrypt        = true
   }
 }
-
-data "terraform_remote_state" "level1" {
-  backend = "s3"
-  config = {
-    bucket = "my-bucket-for-tf-state"
-    key    = "l1/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
