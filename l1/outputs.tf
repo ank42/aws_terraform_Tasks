1303d29a7245fd7ce1cc5cc6324946f46415149f
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2022-01-22 16:36:24 -0500

    Fix module outputs

--- l1/outputs.tf
+++ l1/outputs.tf
@@ -1,9 +1,9 @@
 output "public_subnet_id" {
   description = "Public Subnet ID"
-  value = aws_subnet.Public.*.id
+  value = module.vpc.public_subnet_id
 }
 
 output "vpc_id" {
   description = "VPC ID"
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
